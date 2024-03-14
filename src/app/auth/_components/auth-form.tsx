77c7b76efff9fcf05df833765a4ddf36c900ac35
DiegoFabbrii <diegofabbri@outlook.com>
2024-03-14 18:18:56 -0300

    feat: initial commit

---
+++ src/app/auth/_components/auth-form.tsx
@@ -0,0 +1,39 @@
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export function AuthForm() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-3xl font-bold">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                placeholder="m@example.com"
+                required
+                type="email"
+              />
+            </div>
+            <Button className="w-full">Send Magic Link</Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
