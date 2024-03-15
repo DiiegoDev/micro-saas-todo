2888398353c0bfb160b3c53363325f7896328906
DiegoFabbrii <diegofabbri@outlook.com>
2024-03-14 22:36:41 -0300

    feat: add prisma

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
 });
